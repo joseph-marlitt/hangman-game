51a992fb7b9f7cd15a62b0f23dcad01dbf516b61
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-11-06 14:57:51 -0600

    setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//An array of words that fit the theme
+var hangmanWords = [
+"bouldering",
+"belaying", 
+"sending", 
+"crimp", 
+"lockoff", 
+"gnarley", 
+"bruhbruhbruh", 
+"overhang", 
+"onsighting"
+]
+
+//create an Array of Blanks
