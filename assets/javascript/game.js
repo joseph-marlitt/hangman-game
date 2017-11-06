99ddad68dc9ba373317f13b9ef95113a95e2a245
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-11-06 15:04:21 -0600

    setup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,5 +10,14 @@ var hangmanWords = [
 "overhang", 
 "onsighting"
 ]
+// choose random word
+var gameWord = hangmanWords[Math.floor(hangmanWords.length * Math.random())];
 
+//making the word seperate
+var gameWord = word.split (;
+var arrayOfBlanks = gameWord.map(function(a){
+	rerturn '_'
+}
+
+console.log(gameWord)
 //create an Array of Blanks
