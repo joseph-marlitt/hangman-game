8aaba19f06a09e7ccc5d63e150c4d6a68270bfcb
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-11-09 20:56:18 -0600

    draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,31 +11,34 @@ var hangmanWords = [
 "overhang", 
 "onsighting"
 ]
-
+var randomWord = hangmanWords[Math.floor(hangmanWords.length * Math.random())];
+var splitWord = randomWord.split ("");
 var alreadyGuessed = [];
 var guessesRemaining = 10;
 var points = 0;
 var letters = (/^[a-z]+$/);
-var randomWord = hangmanWords[Math.floor(hangmanWords.length * Math.random())];
-var splitWord = randomWord.split ("");
-var arrayOfBlanks = [];
-var randomWord = hangmanWords[Math.floor(hangmanWords.length * Math.random())];
-	var splitWord = randomWord.split ("");
-// Creates an array of blanks with Splitword
-function startGame(){
 
+var arrayOfBlanks = [];
+// Creates an array of blanks with Splitwor
+
+
+function startGame(){
+	var randomWord = hangmanWords[Math.floor(hangmanWords.length * Math.random())];
+	var splitWord = randomWord.split ("");
+	// Keypress recognition
 	for (i = 0; i < randomWord.length; i++) {
 		arrayOfBlanks[i] = "_";
 		$("#mysteryWord").html("Try and guess me! " + arrayOfBlanks.join(" "));
-	console.log(arrayOfBlanks);
-}
-}
-// Keypress recognition
+		console.log(arrayOfBlanks);
+	}
+
+
 // On keystroke up, loop for matches within word
 document.onkeyup = function(press) {  
+    var realLetter = " ";
     var letterGuessed = (press.key);
     console.log(letterGuessed);
-    var realLetter = " ";
+    
    
 // Check if letter pressed exists in the hidden word.
 	 	
@@ -50,23 +53,29 @@ document.onkeyup = function(press) {
 			} // end replace blank
 		}
 			if(randomWord.indexOf(letterGuessed) < 0) {
-				alreadyGuessed.push(letterGuessed);
+				if (alreadyGuessed.indexOf(letterGuessed) == -1) {
+    				alreadyGuessed.push(letterGuessed);
+    				guessesRemaining --;
+			}
 				console.log(guessesRemaining);
-				guessesRemaining --;
+				
 				$("#guessesRemaining").html("Guesses Remaining: " + guessesRemaining);
 			}
-			if (arrayOfBlanks == randomWord) {
-				$("youwin").html("YOU SENDED THE GNAR!");
+			if (randomWord==arrayOfBlanks.join('')) {
+				$("#youwin").html("YOU SENDED THE GNAR!");
 				points ++;
+				startGame();
+				alreadyGuessed = [];
 			}
 			if (guessesRemaining == 0) {
 				$("#youlose").html("You just fell to your death, nice one loser");
 			}
+
 	$("#guessesRemaining").html("Guesses Remaining: " + guessesRemaining);
 	$("#mysteryWord").html("Try and guess me! " + arrayOfBlanks.join(" "));
 	$("#lettersGuessed").html("Letters Guessed: " + alreadyGuessed);
 	$("#points").html(" Points: " + points);
-}
+}}
 
 	
 
