1189599514276eb1bbae5ca5390ce21d0bc538f1
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-11-09 20:07:01 -0600

    draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,51 +18,57 @@ var points = 0;
 var letters = (/^[a-z]+$/);
 var randomWord = hangmanWords[Math.floor(hangmanWords.length * Math.random())];
 var splitWord = randomWord.split ("");
-var arrayOfBlanks = []
-
+var arrayOfBlanks = [];
+var randomWord = hangmanWords[Math.floor(hangmanWords.length * Math.random())];
+	var splitWord = randomWord.split ("");
 // Creates an array of blanks with Splitword
 function startGame(){
+
 	for (i = 0; i < randomWord.length; i++) {
 		arrayOfBlanks[i] = "_";
-		console.log(arrayOfBlanks);
 		$("#mysteryWord").html("Try and guess me! " + arrayOfBlanks.join(" "));
-	}
+	console.log(arrayOfBlanks);
 }
-console.log(randomWord, splitWord, arrayOfBlanks)
-
+}
 // Keypress recognition
 // On keystroke up, loop for matches within word
 document.onkeyup = function(press) {  
     var letterGuessed = (press.key);
     console.log(letterGuessed);
     var realLetter = " ";
-    alreadyGuessed.push(letterGuessed);
-
+   
 // Check if letter pressed exists in the hidden word.
-		for (i = 0; i < alreadyGuessed.length; i++) 
+	 	
+	for (i = 0; i < alreadyGuessed.length; i++) 
           for(j = 0; j< arrayOfBlanks.length; j++){
+
 			if (letterGuessed == randomWord[j]) {
 				console.log("correct");
 				arrayOfBlanks[j] = letterGuessed;
 				console.log(arrayOfBlanks);
 				$("#mysteryWord").html("Try and guess me! " + arrayOfBlanks.join(" "));
-
 			} // end replace blank
 		}
 			if(randomWord.indexOf(letterGuessed) < 0) {
-				guessesRemaining --;
 				alreadyGuessed.push(letterGuessed);
-				console.log(guessesRemaining)
+				console.log(guessesRemaining);
+				guessesRemaining --;
+				$("#guessesRemaining").html("Guesses Remaining: " + guessesRemaining);
 			}
 			if (arrayOfBlanks == randomWord) {
 				$("youwin").html("YOU SENDED THE GNAR!");
 				points ++;
 			}
 			if (guessesRemaining == 0) {
-				$("youlose")
+				$("#youlose").html("You just fell to your death, nice one loser");
 			}
+	$("#guessesRemaining").html("Guesses Remaining: " + guessesRemaining);
+	$("#mysteryWord").html("Try and guess me! " + arrayOfBlanks.join(" "));
+	$("#lettersGuessed").html("Letters Guessed: " + alreadyGuessed);
+	$("#points").html(" Points: " + points);
+}
 
-	}
+	
 
 	
 
